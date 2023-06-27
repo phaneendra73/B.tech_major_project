e26212af8cc04697aebb02a914b766a5e8dddd0a
phaneendra73 <phaneendra3377@gmail.com>
2023-06-28 01:10:38 +0530

    done

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 const read_channel =
   "https://api.thingspeak.com/channels/2182455/feeds.json?api_key=TCJU8DGD2FFMMCTC&results=2";
-let patient1 = document.getElementById("patient1");
-fetch(read_channel)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    // Handle any errors that occur during the fetch or parsing
-    console.log("Error:", error);
-  });
-//  patient1.innerHTML = `<h2 class="banner-title"></h2>
-//     <p class="banner-info">IMDB ${rating}</p>
-//     <p class="banner-overview">${movie.release_date}<p>
-//     <p class="banner-overview">${movie.overview}<p>
-//     <div class="action-buttons">`;
+
+  patients=document.getElementById("patients");
+
+function fetchDataAndUpdate() {
+  fetch(read_channel)
+    .then((response) => response.json())
+    .then((data) => {
+      // 'data' contains the parsed JSON object
+      // You can access the data and perform operations here
+      var info = data;
+      console.log(info.feeds)
+      patients.innerHTML = `<div class="patients">
+      <h2>Patient 1 Data</h2>
+      <div>Temperature 1= ${info.feeds[0].field1 - 5}</div>
+      <div>ECG 1= ${info.feeds[0].field2}</div>
+      <div>PULSE 1= ${info.feeds[0].field3}</div>
+       <h2>Patient 2 Data</h2>
+      <div>Temperature 1= ${info.feeds[0].field4}</div>
+      <div>ECG 1= ${info.feeds[0].field5}</div>
+      <div>PULSE 1= ${info.feeds[0].field6}</div>
+      </div>`;
+      
+    })
+    .catch((error) => {
+      // Handle any errors that occur during the fetch or parsing
+      console.log("Error:", error);
+    });
+}
+
+// Call the fetchDataAndUpdate function initially
+fetchDataAndUpdate();
+
+// Set up a timer to call the function every 10 seconds
+setInterval(fetchDataAndUpdate, 15000);
